06cba39b0f37f85b4e93ff9bd7f0e01a2b737df6
dev-lincoln <lincoln.dev@outlook.com>
2022-10-08 17:34:00 -0300

    preparação para o merge

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-function menuShow() {
-   let menuMobile = document.querySelector(".mobile-menu");
-   if (menuMobile.classList.contains("open")) {
-      menuMobile.classList.remove("open");
-      document.querySelector(".icon").scr = "./img/icons/menu_white_36dp.svg";
-   } else {
-      menuMobile.classList.add("open");
-   }
-}
